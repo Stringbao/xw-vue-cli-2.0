cd6655df142e383494efbcf7551f3deae07be5bc
wangrj9 <wangrj9@lenovo.com>
2020-07-14 14:32:29 +0800

    2

--- xw-vue-git/webpack/webpack.common.js
+++ xw-vue-git/webpack/webpack.common.js
@@ -56,7 +56,18 @@ module.exports = {
         new HtmlWebpackPlugin({
             title: 'test title',
             filename: 'index.html',
-            template:"./src/index.html",
+            templateContent:`
+            <!DOCTYPE html>
+            <html lang="en">
+                <head>
+                    <meta charset="UTF-8">
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+                    <title>Document</title>
+                </head>
+                <body>
+                    <div id="app"></div> 
+                </body>
+            </html>`,
             chunks:['index','runtime','vendors'],
             inject: true,
             minify: {
