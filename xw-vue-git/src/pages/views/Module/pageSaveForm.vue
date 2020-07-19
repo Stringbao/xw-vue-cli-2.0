3a7cece5fd8553dac879db2547226402331e97b0
zhangxl <zhangxl37@lenovo.com>
2020-07-19 21:21:36 +0800

    css

--- xw-vue-git/src/pages/views/Module/pageSaveForm.vue
+++ xw-vue-git/src/pages/views/Module/pageSaveForm.vue
@@ -2,7 +2,8 @@
     <le-form labelWidth='100' ref="saveForm">
         <div>
             <le-input on required msg="请输入页面名称"
-            label="PageName:" v-model="page.pageName"></le-input>
+                tip="提示: 页面名称必须以.vue结尾"
+                label="PageName:" v-model="page.pageName"></le-input>
             <!-- searchModel的配置 -->
             <div class="configItem">
                 <div class="configItem-title clearfix">
@@ -16,10 +17,10 @@
                             <li class="clearfix" v-for="(item, idx) in page.model" :key="idx">
                                 <div>
                                     <div class="le_form_row_item">
-                                        <le-radio-list label="type:" :data-source="searchModelTypes" 
-                                            display-name="name" display-value="code" 
+                                        <le-local-select label="type:" :data-source="searchModelTypes" 
+                                            display-name="name" display-value="name"
                                             v-model="item.type">
-                                        </le-radio-list>
+                                        </le-local-select>
                                         <le-input label="label:" v-model="item.label"></le-input>
                                         <le-input label="field:" v-model="item.field"></le-input>
                                         <le-button class="fr" type="remove" value="deleteItems" 
@@ -87,6 +88,7 @@ export default {
             datasourceDialog:false,
             searchModelTypes:[
                 {name:"text",code:"text"},
+                {name:"dataTime",code:"dataTime"},
                 {name:"select",code:"select"}
             ],
            
@@ -145,12 +147,17 @@ export default {
             let res = this.$refs.saveForm.validate()
                 .then(res=>{
                     that.page.type = "save";
-                    if(that.action == "create"){
-                        that.addPages(that.page);   
+                    let reg = /^.*\.vue$/; 
+                    if(reg.test(that.page.pageName)){
+                        if(that.action == "create"){ 
+                            that.addPages(that.page);  
+                        }else{
+                            that.updatePages({page:that.page,idx:that.idx});
+                        };
+                        this.$emit("closePageDialog");
                     }else{
-                        that.updatePages({page:that.page,idx:that.idx})
-                    }
-                    this.$emit("closePageDialog");
+                        that.alert.showAlert("error","页面名称必须以.vue结尾");
+                    };
                 })
                 .catch(error => {
                     that.alert.showAlert("error", "请填写所有的必填项");
