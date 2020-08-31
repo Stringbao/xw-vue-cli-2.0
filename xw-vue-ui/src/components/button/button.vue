967cd07d24dacdc4e10d6f50e932e186575b45e4
Stringbao <string_bao@163.com>
2020-08-31 17:44:28 +0800

    3

--- xw-vue-ui/src/components/button/button.vue
+++ xw-vue-ui/src/components/button/button.vue
@@ -61,6 +61,9 @@ export default {
             if(!this.disabled){
                 this.$emit('click')
             }
+        },
+        submit(){
+            this.$emit('submit');
         }
     },
     mounted(){
