0ccbc1b493df0448410f6a9db0031acc6c53d4d9
武鹏 <wupeng@wupengdeMacBook-Pro.local>
2020-11-03 01:04:49 +0800

    3

--- xw-vue-npm/services/test.js
+++ xw-vue-npm/services/test.js
@@ -1,34 +1,4 @@
+const fsTool = require("../tool/fsapi.js");
 
-
-const gitDownload = require('./download.js');
-const command = require("./command.js");
-const NPath = require("./pathStore.js");
-const log = require('../tool/log.js');
-const ora = require('ora');
-const webServer = require("../index.js");
-const Util = require("../tool/util.js");
-
-async function startServer() {
-    await gitDownload(NPath.get());
-    log.success("项目download Success");
-    // install all the packages for git and node
-    let _promise = command.installPackages();
-
-    const spinner = ora('Installing package').start();
-    spinner.start();
-    Promise.all(_promise).then(x=>{
-        spinner.stop();
-        log.success("git and npm packages install completed!");
-        //start web server
-        webServer();
-        //start npm dev mode
-        command.startGitServer();
-    })
-}
-
-if(!Util.getConfigPath()){
-    return;
-}
-
-
-startServer();
+let str = fsTool.file.readFile("./test.txt");
+debugger
